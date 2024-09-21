a55941506d53e7fb60560ce8e34021b34d6fe6d3
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-21 10:23:56 +0300

    updated: alert view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.ismailmesutmujde.kotlinalertview"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.ismailmesutmujde.kotlinalertview"
         minSdk = 24
--- app/src/main/java/com/ismailmesutmujde/kotlinalertview/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinalertview/MainActivity.kt
@@ -1,13 +1,64 @@
 package com.ismailmesutmujde.kotlinalertview
 
+import android.content.DialogInterface
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.ismailmesutmujde.kotlinalertview.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bindingMain : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        val view = bindingMain.root
+        setContentView(view)
+
+        bindingMain.buttonNormal.setOnClickListener {
+
+            val alertDialog = AlertDialog.Builder(this@MainActivity)
+            alertDialog.setMessage("Message")
+            alertDialog.setTitle("Title")
+            alertDialog.setIcon(R.drawable.image)
+
+            alertDialog.setPositiveButton("OK") { dialogInterface, i ->
+                Toast.makeText(applicationContext,"OK was clicked.", Toast.LENGTH_SHORT).show()
+            }
+            alertDialog.setNegativeButton("CANCEL") { dialogInterface, i ->
+                Toast.makeText(applicationContext,"CANCEL was clicked.", Toast.LENGTH_SHORT).show()
+            }
+
+            alertDialog.create().show()
+
+        }
+
+        bindingMain.buttonPrivate.setOnClickListener {
+
+            val design = layoutInflater.inflate(R.layout.alert_design, null)
+            val editTextAlert = design.findViewById(R.id.editTextAlert) as EditText
+            val alertDialog = AlertDialog.Builder(this@MainActivity)
+            alertDialog.setMessage("Message")
+            alertDialog.setTitle("Title")
+            alertDialog.setIcon(R.drawable.image)
+
+            alertDialog.setView(design)
+
+            alertDialog.setPositiveButton("SAVE") { dialogInterface, i ->
+                val incomingData = editTextAlert.text.toString()
+
+                Toast.makeText(applicationContext,"Incoming Data : $incomingData", Toast.LENGTH_SHORT).show()
+            }
+            alertDialog.setNegativeButton("CANCEL") { dialogInterface, i ->
+                Toast.makeText(applicationContext,"CANCEL was clicked.", Toast.LENGTH_SHORT).show()
+            }
+
+            alertDialog.create().show()
+
+        }
 
     }
 }
